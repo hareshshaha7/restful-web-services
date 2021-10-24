7ed172cc02e283fe5478df1a6d53fe0e72178cec
Haresh Shaha <haresh.shaha7@gmail.com>
2021-10-24 11:43:13 +0530

    Added some path variable example

--- src/main/java/com/haresh/spring/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/haresh/spring/restfulwebservices/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.haresh.spring.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 public class HelloWorldController {
@@ -14,5 +17,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/name/{name}")
+    public String HelloMyWorld(@PathVariable String name) {
+        return "Hello " + name;
     }
 }
